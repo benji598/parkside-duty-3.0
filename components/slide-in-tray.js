bc44d2da2d40584f8fd5b7a32f18fab0a119c2ba
Ben Hewart <>
2024-04-01 15:29:25 +0100

    swipe gesture works on the slide in tray as well now

--- components/slide-in-tray.js
+++ components/slide-in-tray.js
@@ -20,13 +20,43 @@ SlideInTrayTemplate.innerHTML = /*html*/ `
         transform: translateX: 0;
     }
 
+
+    .inner-wrapper {
+        background-color: var(--color-white);
+        width: 100%;
+        margin: 1rem;
+        border-radius: 1rem;
+    }
+
+    .gesture-bar {
+        border: 2px solid var(--color-dark-grey);
+        border-radius: var(--btn-radius);
+        margin-left: 1rem;
+        height: 100%;
+        max-height: 50px;
+        justify-content: center;
+        margin-bottom: auto;
+        margin-top: auto;
+    }
+
+    .flex-box {
+        display: flex;
+    }
+
     .dark {
         position: fixed;
         top: 0;
         left: 0;
-        background-color: rgba(0, 0, 0, 0.5);
         width: 100vw;
         height: 100vh;
+        background-color: rgba(0, 0, 0, 0.4);
+        backdrop-filter: blur(4px);
+        -webkit-backdrop-filter: blur(4px);
+    }
+
+
+    .dragging {
+        transition: none;
     }
 
     logout-btn {
@@ -37,7 +67,12 @@ SlideInTrayTemplate.innerHTML = /*html*/ `
 
 <div class="tray">
     <close-btn></close-btn>
-    <logout-btn></logout-btn>
+    <div class="flex-box">
+        <div class="gesture-bar"></div>
+        <div class="inner-wrapper">
+            <logout-btn></logout-btn>
+        </div>
+    </div>
 </div>
 <div class="overlay"></div>
 <slot></slot>
@@ -45,39 +80,84 @@ SlideInTrayTemplate.innerHTML = /*html*/ `
 `;
 
 class SlideInTray extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.attachShadow({
+      mode: 'open',
+    });
+    this.shadowRoot.appendChild(SlideInTrayTemplate.content.cloneNode(true));
 
-        this.attachShadow({
-            mode: 'open',
-        });
-        this.shadowRoot.appendChild(SlideInTrayTemplate.content.cloneNode(true));
+    // Initial touch position
+    // this.initialX = null;
+  }
+
+  connectedCallback() {
+    document.addEventListener('slide-in', () => {
+      setTimeout(() => this.slideIn(), 10);
+    });
+
+    this.SlideIn = this.shadowRoot.querySelector('.tray');
+    this.overlay = this.shadowRoot.querySelector('.overlay');
+    this.shadowRoot.querySelector('close-btn').addEventListener('click', this.closeSlideInTray.bind(this));
+
+    this.SlideIn.addEventListener('touchstart', this.handleTouchStart.bind(this), false);
+    this.SlideIn.addEventListener('touchmove', this.handleTouchMove.bind(this), false);
+    this.SlideIn.addEventListener('touchend', this.handleTouchEnd.bind(this), false);
+  }
+
+  disconnectedCallback() {
+    this.SlideIn.removeEventListener('touchstart', this.handleTouchStart);
+    this.SlideIn.removeEventListener('touchmove', this.handleTouchMove);
+    this.SlideIn.removeEventListener('touchend', this.handleTouchEnd);
+  }
+
+  slideIn() {
+    this.SlideIn.classList.add('slide-in');
+    this.overlay.classList.add('dark');
+  }
+
+  closeSlideInTray() {
+    this.SlideIn.style.transform = '';
+    this.SlideIn.classList.remove('slide-in');
+    this.overlay.classList.remove('dark');
+    setTimeout(() => {
+      this.remove();
+    }, 350);
+  }
+
+  handleTouchStart(e) {
+    this.initialX = e.touches[0].clientX;
+  }
+
+  handleTouchMove(e) {
+    if (this.initialX === null) {
+      return;
     }
 
-    connectedCallback() {
-        document.addEventListener('slide-in', (el) => {
-            console.log(el);
-            setTimeout(() => {
-                this.slideIn();
-            }, 10);
-        });
-        this.close();
-    }
+    e.preventDefault();
 
-    slideIn() {
-        this.shadowRoot.querySelector('.tray').classList.add('slide-in');
-        this.shadowRoot.querySelector('.overlay').classList.add('dark');
-    }
+    let currentX = e.touches[0].clientX;
+    let diffX = currentX - this.initialX;
 
-    close() {
-        this.shadowRoot.querySelector('close-btn').addEventListener('click', () => {
-            this.shadowRoot.querySelector('.tray').classList.remove('slide-in');
-            this.shadowRoot.querySelector('.overlay').classList.remove('dark');
-            setTimeout(() => {
-                this.remove('slide-in-tray');
-            }, 350);
-        });
+    if (diffX > 0) {
+      this.SlideIn.classList.add('dragging');
+      this.SlideIn.style.transform = `translateX(${diffX}px)`;
     }
+  }
+
+  handleTouchEnd(e) {
+    let currentX = e.changedTouches[0].clientX;
+    let diffX = currentX - this.initialX;
+
+    this.SlideIn.classList.remove('dragging');
+
+    if (diffX > this.SlideIn.clientWidth * 0.35) {
+      this.closeSlideInTray();
+    } else {
+      this.SlideIn.classList.add('slide-in'); // Reapply the class that keeps the tray visible
+      this.SlideIn.style.transform = 'translateX(0)'; // Correctly position the tray on-screen
+    }
+  }
 }
 
 customElements.define('slide-in-tray', SlideInTray);
