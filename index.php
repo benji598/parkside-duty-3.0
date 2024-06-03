3f3dc0d6a2a1d1db18cc2a9663c7f7e53e3e6d9a
Ben Hewart <>
2024-06-04 00:56:59 +0100

    no message

--- index.php
+++ index.php
@@ -40,7 +40,6 @@ include 'header.php';
 
     <header-info title="Parkside Duties" subtitle="Choose a Duty">
     </header-info>
-    test
     <main-wrapper>
         <duty-btn-grid></duty-btn-grid>
     </main-wrapper>
